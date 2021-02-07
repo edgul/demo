fb7812297553ee7062e2601596962d5ae82e1d85
edgul <ed.guloien@gmail.com>
2021-02-07 18:20:48 -0500

    Added memoise rand and fact tests

--- cpp/memoize/main.cpp
+++ cpp/memoize/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
 #include <map>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
 
 // helpers
 int sum(int i, int j) { std::cout << "Calculating sum for: " << i << " + " << j << std::endl; return i + j; }
 int succ(int i) { std::cout << "Calculating succ for: " << i << std::endl; return i + 1; }
 double succDouble(int i) { std::cout << "Caluating succDouble for: " << i << std:: endl; return static_cast<double>(i+1)+0.5; }
+unsigned long long fact(int n)
+{
+    unsigned long long result = 1;
+    for (int i = 2; i <= n; i++)
+        result *= i;
+    return result;
+}
 
 // TODO: add perfect forwarding
-
-// #1 memoise : returns memoized version of argument function
+// #1 memoize : returns memoized version of argument function
 template <class F > auto memoize(F f)
 {
     auto mem_f = [f](auto... args) { 
@@ -25,20 +34,74 @@ template <class F > auto memoize(F f)
     return mem_f;
 }
 
+unsigned long time_since_epoch_count()
+{
+    return std::chrono::system_clock::now().time_since_epoch().count();
+}
 
+// #2 memoize rand : 
+// * It "works" in a sense that the randInt function is not called a second time on the same inputs. 
+// * It doesn't work in a sense that the randInt function no longer performs its job
+void test_memoize_rand()
+{
+    auto randInt = [](){
+        srand(time_since_epoch_count());
+        std::cout << "Calculating random int" << std::endl;
+        return rand() % 100;
+    };
+    
+    auto mem_rand_int = memoize(randInt);
+    std::cout << mem_rand_int() << std::endl; // 
+    std::cout << mem_rand_int() << std::endl; // prints same as above
+}
+
+// #3 memoize rand seeded :
+// * random functionality works
+// * but not really a point in memoizing since input should never be the same so random function will always be called
+void test_memoize_rand_seeded()
+{
+    auto randInt = [](int seed){
+        srand(seed);
+        std::cout << "Calculating random int" << std::endl;
+        return rand() % 100;
+    };
+
+    auto mem_rand_int_seeded = memoize(randInt);
+    std::cout << mem_rand_int_seeded(time_since_epoch_count()) << std::endl; 
+    std::cout << mem_rand_int_seeded(time_since_epoch_count()) << std::endl; // prints different than above
+}
+
+void benchmark_fact(int n)
+{
+    auto mem_fact = memoize(fact);
+    unsigned long start = time_since_epoch_count();
+    fact(n);
+    unsigned long end = time_since_epoch_count();
+    unsigned long total = end - start;
+    std::cout << "Run time in counts: " << total << std::endl;
+}
+
+void test_memoize_fact()
+{
+    benchmark_fact(50); // 1155
+    benchmark_fact(50); // 731
+
+    // TODO: huge time diff (benchmark error, copy overhead)
+    //benchmark_mem_fact(50); // 17973
+    //benchmark_mem_fact(50); // 2531
+}
 
 void test_memoize();
 
 int main(int argc, char *argv[])
 {
-    test_memoize();
+    test_memoize_fact();
     return 0;
 }
 
 void test_memoize()
 {
     auto memoizedSucc = memoize(succ);
-    std::cout << memoizedSucc(5) << std::endl;
     std::cout << memoizedSucc(5) << std::endl;
     std::cout << memoizedSucc(5) << std::endl;
    
