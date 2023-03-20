c1cb7ea06623cce60f7ecd1dac020e5db4d5d964
Ed Guloien <ed.guloien@gmail.com>
2023-03-19 21:23:25 -0400

    update maze generator: added high level algorithm for adding new dead-end paths to the maze tree

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -3,18 +3,13 @@ use maze::*;
 
 fn main() {
     println!("Hello, world!");
-    let m = Maze::new(20, 10);
-    m.print();
+    // let m = Maze::new(20, 10);
+    // m.print();
 
-    let mut n = Node::new(0, 0);
-    n.children.push(Node::new(0, 1));
-    // probably excessive copying
-    let mut n1 = Node::new(1, 0);
-    n1.children.push(Node::new(1, 1));
+    let mut n = Node::new(&Pt::new(0, 0));
+    let n1 = Node::new(&Pt::new(1, 0));
     n.children.push(n1);
-    n.print();
 
-    let found = n.get(Pt::new(1, 0));
-    found.unwrap().add(Node::new(2, 2));
-    n.print();
+    let n2 = n.clone();
+    n2.print();
 }
--- maze/src/maze.rs
+++ maze/src/maze.rs
@@ -1,42 +1,79 @@
+use rand::Rng;
+
 pub struct Maze {
     width: usize,
     height: usize,
-    tree: Option<Node>,
+    tree: Node,
     table: Vec<Vec<Cell>>,
 }
 
 impl Maze {
     pub fn new(width: usize, height: usize) -> Self {
-        let temp_self = Self {
+        let start = Pt::new(0, height / 2);
+        let end = Node::new(&Pt::new(width - 1, height / 2));
+
+        let mut temp_self = Self {
             width,
             height,
-            tree: None,
+            tree: Node::new(&start),
             table: vec![vec![Cell::new(); width]; height],
         };
 
-        // make happy path
-        let start = Node::new(0, height / 2);
-        let end = Node::new(width - 1, height / 2);
+        // add happy path to the tree
+        let all_paths = temp_self.valid_paths(&start, &end.get_pt());
+        let random_index = rand::thread_rng().gen_range(0..all_paths.len());
+        temp_self.tree = all_paths[random_index].clone();
 
-        let all_paths = temp_self.valid_paths(start.get_pt(), end.get_pt());
+        // make new paths and connect to the tree
+        let mut empty_cells = temp_self.empty_cells();
+        while empty_cells.len() > 0 {
+            // start from random empty cell
+            let r_empty_cell_index = rand::thread_rng().gen_range(0..empty_cells.len());
+            let empty_pt = empty_cells[r_empty_cell_index].clone();
 
-        // todo: connect all empty nodes to tree
+            // grab random joinable tree node
+            let joinable_pts = temp_self.joinable_nodes(&empty_pt);
+            let random_index = rand::thread_rng().gen_range(0..joinable_pts.len());
+            let tree_pt = joinable_pts[random_index].clone();
+
+            // grab random path of all paths between start and tree node
+            let paths = temp_self.valid_paths(&empty_pt, &tree_pt);
+            let random_path_index = rand::thread_rng().gen_range(0..paths.len());
+            let mount_node = temp_self.tree.get(tree_pt).unwrap();
+
+            // add the path to the tree
+            mount_node.add(paths[random_path_index].clone());
+            empty_cells = temp_self.empty_cells();
+        }
 
         // todo: populate the table
 
         temp_self
     }
 
-    fn valid_paths(&self, src: Pt, dest: Pt) -> Vec<Vec<Pt>> {
+    // todo
+    fn valid_paths(&self, src: &Pt, dest: &Pt) -> Vec<Node> {
         let paths = vec![];
-        // todo
         paths
     }
 
-    fn joinable_nodes(&self) -> Vec<Pt> {
+    // todo
+    fn joinable_nodes(&self, from_pt: &Pt) -> Vec<Pt> {
         let pts = vec![];
-        // todo
         pts
+    }
+
+    fn empty_cells(&mut self) -> Vec<Pt> {
+        let mut v = vec![];
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let pt = Pt::new(x as usize, y as usize);
+                if self.tree.get(pt.clone()).is_none() {
+                    v.push(pt);
+                }
+            }
+        }
+        v
     }
 
     pub fn print(&self) {
@@ -73,17 +110,28 @@ impl Pt {
     pub fn new(x: usize, y: usize) -> Self {
         Self { x, y }
     }
+
+    // seems to be bug in rust analyser(?),
+    // derive Clone with pt.clone() calls compiles
+    // implementing to shut the linter up
+    pub fn clone(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y,
+        }
+    }
 }
 
+#[derive(Clone)]
 pub struct Node {
     pt: Pt,
     pub children: Vec<Node>,
 }
 
 impl Node {
-    pub fn new(x: usize, y: usize) -> Self {
+    pub fn new(pt: &Pt) -> Self {
         Self {
-            pt: Pt { x, y },
+            pt: Pt { x: pt.x, y: pt.y },
             children: vec![],
         }
     }
