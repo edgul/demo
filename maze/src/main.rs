608fef04a71584a3e5b695a3d574f2a20bb338ab
Ed Guloien <ed.guloien@gmail.com>
2023-03-22 00:33:38 -0400

    Changed approach to 'grow' into all the available spaces

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -2,14 +2,6 @@ mod maze;
 use maze::*;
 
 fn main() {
-    println!("Hello, world!");
-    // let m = Maze::new(20, 10);
-    // m.print();
-
-    let mut n = Node::new(&Pt::new(0, 0));
-    let n1 = Node::new(&Pt::new(1, 0));
-    n.children.push(n1);
-
-    let n2 = n.clone();
-    n2.print();
+    let m = Maze::new(20, 10);
+    m.print();
 }
--- maze/src/maze.rs
+++ maze/src/maze.rs
@@ -2,89 +2,181 @@ use rand::Rng;
 
 pub struct Maze {
     width: usize,
-    height: usize,
-    tree: Node,
+    //height: usize,
     table: Vec<Vec<Cell>>,
+    explored_grid: Vec<Vec<bool>>,
+    links: Vec<(Pt,Pt)>,
 }
 
 impl Maze {
     pub fn new(width: usize, height: usize) -> Self {
         let start = Pt::new(0, height / 2);
-        let end = Node::new(&Pt::new(width - 1, height / 2));
+        // let end = Node::new(&Pt::new(width - 1, height / 2));
 
         let mut temp_self = Self {
             width,
-            height,
-            tree: Node::new(&start),
+            //height,
             table: vec![vec![Cell::new(); width]; height],
+            explored_grid: vec![vec![false; width]; height],
+            links: vec![],
         };
 
-        // add happy path to the tree
-        let all_paths = temp_self.valid_paths(&start, &end.get_pt());
-        let random_index = rand::thread_rng().gen_range(0..all_paths.len());
-        temp_self.tree = all_paths[random_index].clone();
-
-        // make new paths and connect to the tree
-        let mut empty_cells = temp_self.empty_cells();
-        while empty_cells.len() > 0 {
-            // start from random empty cell
-            let r_empty_cell_index = rand::thread_rng().gen_range(0..empty_cells.len());
-            let empty_pt = empty_cells[r_empty_cell_index].clone();
-
-            // grab random joinable tree node
-            let joinable_pts = temp_self.joinable_nodes(&empty_pt);
-            let random_index = rand::thread_rng().gen_range(0..joinable_pts.len());
-            let tree_pt = joinable_pts[random_index].clone();
-
-            // grab random path of all paths between start and tree node
-            let paths = temp_self.valid_paths(&empty_pt, &tree_pt);
-            let random_path_index = rand::thread_rng().gen_range(0..paths.len());
-            let mount_node = temp_self.tree.get(tree_pt).unwrap();
-
-            // add the path to the tree
-            mount_node.add(paths[random_path_index].clone());
-            empty_cells = temp_self.empty_cells();
-        }
-
-        // todo: populate the table
-
+        temp_self.create_paths(&start);
+        temp_self.populate_table();
         temp_self
     }
 
-    // todo
-    fn valid_paths(&self, src: &Pt, dest: &Pt) -> Vec<Node> {
-        let paths = vec![];
-        paths
+    fn right(src: &Pt, dest: &Pt) -> bool {
+       src.x + 1 == dest.x && src.y == dest.y
+    }
+    fn down(src: &Pt, dest: &Pt) -> bool {
+       src.x == dest.x && src.y + 1 == dest.y
     }
 
-    // todo
-    fn joinable_nodes(&self, from_pt: &Pt) -> Vec<Pt> {
-        let pts = vec![];
-        pts
+    fn adjacent(&self, pt: &Pt) -> Vec<Pt> {
+        let mut v = Vec::new();
+        if pt.x > 0 {
+            v.push(Pt::new(pt.x - 1, pt.y));
+        }
+        if pt.x < self.explored_grid[0].len() - 1 {
+            v.push(Pt::new(pt.x + 1, pt.y));
+        }
+        if pt.y > 0 {
+            v.push(Pt::new(pt.x, pt.y - 1));
+        }
+        if pt.y < self.explored_grid.len() - 1 {
+            v.push(Pt::new(pt.x, pt.y + 1));
+        }
+        v
     }
 
-    fn empty_cells(&mut self) -> Vec<Pt> {
-        let mut v = vec![];
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let pt = Pt::new(x as usize, y as usize);
-                if self.tree.get(pt.clone()).is_none() {
-                    v.push(pt);
-                }
+    fn adjacent_unexplored(&self, pt: &Pt) -> Vec<Pt> {
+        let mut v = Vec::new();
+        let adjacent_pts = self.adjacent(pt);
+        for p in &adjacent_pts {
+            if !self.explored_grid[p.y][p.x] {
+                v.push(p.clone());
             }
         }
         v
     }
 
+    #[cfg(Test)]
+    fn overwrite_links_for_test(&mut self) {
+        self.links = vec![];
+        self.links.push((Pt::new(0,1), Pt::new(0,0)));
+        self.links.push((Pt::new(0,1), Pt::new(1,1)));
+        self.links.push((Pt::new(0,1), Pt::new(0,2)));
+        self.links.push((Pt::new(0,2), Pt::new(0,3)));// shouldn't affect 
+        
+        self.links.push((Pt::new(2,1), Pt::new(2,0)));
+        self.links.push((Pt::new(2,1), Pt::new(2,2)));
+    }
+
+    fn adjacent_explored(&self, pt: &Pt) -> Vec<Pt> {
+        let mut v = Vec::new();
+        let adjacent_pts = self.adjacent(pt);
+        for p in &adjacent_pts {
+            if self.explored_grid[p.y][p.x] {
+                v.push(p.clone());
+            }
+        }
+        v
+    }
+
+    #[cfg(Test)]
+    fn print_explored_grid(&self) {
+        for row in &self.explored_grid {
+            for b in row {
+                print!("{}", b);
+            }
+            println!("");
+        }
+    }
+
+    fn create_paths(&mut self, src: &Pt) {
+        let mut ready_for_exploring: Vec<Pt> = Vec::new();
+
+        // init our exploration queue
+        self.explored_grid[src.y][src.x] = true;
+        ready_for_exploring.append(&mut self.adjacent_unexplored(src));
+
+        while !ready_for_exploring.is_empty() {
+            let ready_index = rand::thread_rng().gen_range(0..ready_for_exploring.len());
+            let new_pt = ready_for_exploring.remove(ready_index);
+
+            let previously_explored = self.adjacent_explored(&new_pt);
+            let previous_index = rand::thread_rng().gen_range(0..previously_explored.len());
+            let prev_pt = previously_explored[previous_index].clone();
+            
+            self.explored_grid[new_pt.y][new_pt.x] = true;
+            self.links.push((prev_pt, new_pt.clone()));
+
+            let adj_un = &mut self.adjacent_unexplored(&new_pt);
+            for i in adj_un {
+                let mut found = false;
+                for j in &mut ready_for_exploring {
+                    if i == j {
+                        found = true;
+                        break;
+                    }
+                }
+                if !found { ready_for_exploring.push(i.clone()); }
+            }
+        }
+    }
+
+    #[cfg(Test)]
+    fn print_links(&self) {
+        for (pt1,pt2) in &self.links {
+            println!("link: ({},{}), ({},{})", pt1.x, pt1.y, pt2.x,pt2.y);
+        }
+    }
+
+    fn populate_table(&mut self) {
+        // use links to populate the table
+        for y in 0..self.table.len() {
+            for x in 0..self.table[y].len() {
+                let cell = &mut self.table[y][x];
+                let cell_pt = Pt::new(x,y);
+                for (pt1,pt2) in &self.links {
+                    if &cell_pt == pt1 {
+                        if Maze::right(&cell_pt, pt2) { cell.right = true; }
+                        if Maze::down(&cell_pt, pt2) { cell.down = true; }
+                    } else if &cell_pt == pt2 {
+                        if Maze::right(&cell_pt, pt1) { cell.right = true; }
+                        if Maze::down(&cell_pt, pt1) { cell.down = true; }
+                    } 
+                }
+            }
+        }
+    }
+
+    // todo: add start and end markings to drawing
     pub fn print(&self) {
-        println!("Maze::print()");
+        print!("-{}", "-".repeat(self.width*2-1)); // top border
+        println!("-");
+        for row in &self.table {
+            print!("|"); // left border 1
+            for (i, c) in row.iter().enumerate() {
+                if i < row.len() {
+                    if c.right { print!("  ")}
+                    else { print!(" |");}
+                }
+            }
+            println!("");
+            print!("-"); // left border 2
+            for c in row {
+                if c.down { print!(" -")}
+                else { print!("--");}
+            }
+            println!(""); // right border
+        }
     }
 }
 
 #[derive(Clone)]
 struct Cell {
-    left: bool,
-    up: bool,
     right: bool,
     down: bool,
 }
@@ -92,18 +184,16 @@ struct Cell {
 impl Cell {
     pub fn new() -> Self {
         Self {
-            left: false,
-            up: false,
             right: false,
             down: false,
         }
     }
 }
 
-#[derive(Eq, PartialEq, Clone)]
+#[derive(Eq, PartialEq, Clone, Debug)]
 pub struct Pt {
-    x: usize,
-    y: usize,
+    pub x: usize,
+    pub y: usize,
 }
 
 impl Pt {
@@ -122,55 +212,3 @@ impl Pt {
     }
 }
 
-#[derive(Clone)]
-pub struct Node {
-    pt: Pt,
-    pub children: Vec<Node>,
-}
-
-impl Node {
-    pub fn new(pt: &Pt) -> Self {
-        Self {
-            pt: Pt { x: pt.x, y: pt.y },
-            children: vec![],
-        }
-    }
-
-    pub fn add(&mut self, n: Node) {
-        self.children.push(n);
-    }
-
-    pub fn get(&mut self, pt: Pt) -> Option<&mut Node> {
-        if pt == self.pt {
-            return Some(self);
-        }
-
-        for c in &mut self.children {
-            if let Some(n) = c.get(pt.clone()) {
-                return Some(n);
-            }
-        }
-        None
-    }
-
-    pub fn get_pt(&self) -> Pt {
-        self.pt.clone()
-    }
-
-    pub fn print(&self) {
-        self.print_inner(0);
-    }
-
-    fn print_inner(&self, indent: usize) {
-        let indent_string = " ";
-        println!(
-            "{}({},{})",
-            indent_string.repeat(indent),
-            self.pt.x,
-            self.pt.y
-        );
-        for c in &self.children {
-            c.print_inner(indent + 1);
-        }
-    }
-}
