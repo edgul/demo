6d5db9aead49ae1ee5e2fe75e80031cbc9eb2d13
edgul <ed.guloien@gmail.com>
2025-11-30 20:53:36 -0500

    added a basic cpp project that statically and dynamically links two small rust libraries

---
+++ ffi-playground/dynlib/src/lib.rs
@@ -0,0 +1,6 @@
+
+#[allow(dead_code)]
+#[unsafe(no_mangle)]
+pub extern "C" fn hello_dyn() -> () {
+    println!("Hello, dynamic world!");
+}
---
+++ ffi-playground/statlib/src/lib.rs
@@ -0,0 +1,7 @@
+
+#[allow(dead_code)]
+#[unsafe(no_mangle)]
+pub extern "C" fn hello_stat() -> () {
+    println!("Hello, static world!");
+}
+
